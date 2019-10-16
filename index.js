e73e0e0e91e1a3bc2ebf37cfacea89eaf0f5f55e
Alex Mejicanos <alexmejicanos@outlook.com>
2019-10-15 22:40:23 -0600

    Definir endpoint que retorne un JSON

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/healt', (erq, res) => {
 
 app.get('/info', (req, res) => {
     res.send('Soy el proyecto node-server');
-})
+});
+
+app.get('/json', (req, res) => {
+    res.json({
+        app: 'node-server',
+        version: '1.0.0',
+    });
+});
 
 app.listen(3000, () => console.log('La aplicación esta viva....'));
